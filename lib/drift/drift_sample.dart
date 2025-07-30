bbe7f4f9f6f91244694801b1a44cd8a143ece75f
KSTA <>
2025-07-30 13:26:47 +0630

    Add drift sample

---
+++ lib/drift/drift_sample.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_storage_examples/drift/drift_database.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = AppDatabase();
+
+  await database
+      .into(database.todoItems)
+      .insert(
+        TodoItemsCompanion.insert(
+          title: 'todo: finish drift setup',
+          content: 'We can now write queries and define our own tables.',
+        ),
+      );
+  List<TodoItem> allItems = await database.select(database.todoItems).get();
+
+  print('items in database: $allItems');
+}
