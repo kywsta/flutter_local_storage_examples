d1ec2a3e684b33edbb9c81030cbce3b3b3024cc9
KSTA <>
2025-07-30 13:48:49 +0630

    Add query in drift sample

--- lib/drift/drift_sample.dart
+++ lib/drift/drift_sample.dart
@@ -14,7 +14,11 @@ void main() async {
           content: 'We can now write queries and define our own tables.',
         ),
       );
+
   List<TodoItem> allItems = await database.select(database.todoItems).get();
 
   print('items in database: $allItems');
+
+  List<TodoItem> item = await (database.select(database.todoItems)..where((table) => table.id.equals(2))).get();
+  print('item with id 2: $item');
 }
